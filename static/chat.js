baf4f387a20c766f4c235240f2819372998278f4
Poison <dead@poision.com>
2015-12-26 02:14:25 -0800

    Adding restriction on server channel with appropriate meta message support

--- static/chat.js
+++ static/chat.js
@@ -54,23 +54,41 @@
       currentGroup: {name: '', messages: []},
       isConnected: false,
     },
+
     ready: function (argument) {
       this.transport = new core.Transport();
       this.transport.events.on('connected', this.onConnected);
       this.transport.events.on('disconnected', this.onDisconnected);
       this.transport.events.on('message', this.onMessage);
       this.transport.events.on('joined', this.onJoin);
+      this.transport.events.on('switch', this.onSwitch);
     },
+
     methods: {
       connect: function () {
         this.transport.connect();
       },
 
       sendMessage: function (msg) {
+        if (this.currentGroup.name == this.defaultGroup && msg[0] != "/"){
+          this.appendMetaMessage(
+            "You can only send a command here ...\n"+
+            "Valid commands are: \n"+
+            "/join <group_name> to join a group\n"+
+            "/nick <new_name> for changing your nick\n"+
+            "/switch <group_name> to switch to a joined group\n"
+          );
+          return;
+        }
+
         this.transport.send(this.currentGroup.name, msg);
       },
 
       onMessage: function (m) {
+        if (!this.defaultGroup) {
+          this.defaultGroup = m.from;
+        }
+
         this.appendMessage(m);
       },
 
@@ -82,7 +100,18 @@
         this.$set('isConnected', false);
       },
 
-      onJoin: function (group) {
+      onJoin: function (joinInfo) {
+        groupsLog[joinInfo.to] = groupsLog[joinInfo.to] || [];
+      },
+
+      onSwitch: function (group) {
+        if (!groupsLog[group]) {
+          alert('You have not joined group '+group);
+          return;
+        }
+
+        this.$set('currentGroup.name', group);
+        this.$set('currentGroup.messages', groupsLog[group]);
       },
 
       appendMessage: function (m) {
@@ -95,10 +124,11 @@
 
         var grouplog = groupsLog[m.to];
         grouplog.push(m);
+        this.$broadcast('new-message', m);
+      },
 
-        if (this.currentGroup.name == m.to) {
-          this.$broadcast('new-msg', m);
-        }
+      appendMetaMessage: function (msg) {
+        this.currentGroup.messages.push({isMeta: true, msg: msg});
       }
     },
   });
